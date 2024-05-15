28957f2ac9ab46e747e2ffe3d8d7aad0e6dc45cc
Random people <98900217+justaguy6@users.noreply.github.com>
2024-05-15 10:00:17 -0300

    Update StageData.hx

--- source/data/StageData.hx
+++ source/data/StageData.hx
@@ -63,7 +63,7 @@ class StageData
 	public static function getStageFile(stage:String):StageFile
 	{
 		var rawJson:String = null;
-		var path:String = Paths.getPreloadPath('stages/' + stage + '.json');
+		var path:String = SUtil.getStorageDirectory() + Paths.getPreloadPath('stages/' + stage + '.json');
 
 		if (FileSystem.exists(path))
 		{
